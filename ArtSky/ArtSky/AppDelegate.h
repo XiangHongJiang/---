239b0cca8465c78291689ed324bc2e2f0972bc9e
xianghongjiang <632517555@qq.com>
2016-05-31 18:04:41 +0800

    ArtSky重构准备工程建立

---
+++ ArtSky/ArtSky/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArtSky
+//
+//  Created by JXH on 16/5/31.
+//  Copyright © 2016年 JXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
